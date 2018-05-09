2227adb59046ba92113419eed09b84b7f1e7123f
Christopher Tunnell <ctunnell@nikhef.nl>
2018-05-09 16:56:40 +0200

    First functional commit

--- ripa/cli.py
+++ ripa/cli.py
@@ -3,14 +3,47 @@
 """Console script for ripa."""
 import sys
 import click
+import subprocess
+import requirements
 
 
 @click.command()
-def main(args=None):
+@click.option('--file', prompt='Requirements file',
+              type=click.File('r'),
+              default='requirements.txt',
+              help='The name of the requirements fiile')
+def main(file):
     """Console script for ripa."""
-    click.echo("Replace this message by putting your code into "
-               "ripa.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+
+    # For each requirement, determine if in Anaconda                                                                        
+    pip_todo = {}
+    
+    for req in requirements.parse(file):
+        command = 'conda install -y %s=%s' % (req.name, req.specs[0][-1])
+
+        try:
+            output = subprocess.check_output(command,
+                                             stderr=subprocess.STDOUT,
+                                             shell=True,
+                                             universal_newlines=True)
+        except subprocess.CalledProcessError as exc:
+            command2 = 'pip install -q %s' % req.line
+            pip_todo[req.name] = command2
+        else:
+            click.echo('Anaconda: %s' % req.name)
+
+    # Do rest                                                                                                               
+    for name, command2 in pip_todo.items():
+        try:
+            output = subprocess.check_output(command2,
+                                             stderr=subprocess.STDOUT,
+                                             shell=True,
+                                             universal_newlines=True)
+        except:
+            click.ClickException('%s: Failed with pip too' % name)
+        else:
+            click.echo('pip: %s' % name)
+
     return 0
 
 
--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', 
+                'requirements-parser'
+                ]
 
 setup_requirements = [ ]
 
